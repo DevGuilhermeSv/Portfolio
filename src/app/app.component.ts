047c7d57ef215834e3b1fc24eba1832c61386e5c
Guilherme <gui.2469@gmail.com>
2023-03-22 15:01:41 -0300

    showing element information

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
-import { art } from './articles';
+import { Articles } from './articles';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,5 +8,18 @@ import { art } from './articles';
 })
 export class AppComponent {
   title = 'portfolioAngular';
-  articles = art;
+  articles: Articles[] = [
+    {
+        id: 1,
+        title:"Gerando e aplicando migrations com .Net 6",
+        data: "22/03",
+        description:"Como gerar migrations a partidar de um modelo e aplicar para um banco de dados relacional"
+    },
+    {
+        id: 2,
+        title:"Gerando e aplicando migrations com .Net 6",
+        data: "22/03",
+        description:"Como gerar migrations a partidar de um modelo e aplicar para um banco de dados relacional"
+    }
+];
 }
