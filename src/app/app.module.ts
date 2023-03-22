468ecf387404de3320ebe2d2df5104114539841f
Guilherme <gui.2469@gmail.com>
2023-03-22 10:49:13 -0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ElementListComponent } from './element-list/element-list.component';
+import { HeaderComponent } from './header/header.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ElementListComponent,
+    HeaderComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule
