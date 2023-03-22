27ef9245693025d5cfd853e5aa2f3cac3b59a49c
Guilherme <gui.2469@gmail.com>
2023-03-22 16:56:38 -0300

    applied article link style

--- src/app/article-link/article-link.component.ts
+++ src/app/article-link/article-link.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-article-link',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./article-link.component.css']
 })
 export class ArticleLinkComponent {
-
+@Input() topic!: string;
 }
