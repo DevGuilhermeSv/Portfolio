400470d7e72295910be77b8143151a7c7d703966
Guilherme <gui.2469@gmail.com>
2023-03-27 11:43:11 -0300

    update last article

--- src/app/Components/Articles/Articles.ts
+++ src/app/Components/Articles/Articles.ts
@@ -8,9 +8,10 @@ export const articlesList: Articles[] = [
     description: "Quem sou, oque faço e quais as minhas experiências. Falerei um pouco sobre mim e sobre este meu novo espaço...",
     img: "",
     content: `
-    <div class="content"> Ja fazia muito tempo que eu planejava atualizar meu portólio, e tambem sempre tive vontade de apresentar para as pessoas os 
-    novos conceitos e tecnologias que eu aprendia e aplicava nos meu projetos. Então, "juntando o util ao agradavel" aqui esta meu portfólio novo! 
-    Aqui não apenas apresenta informações sobre mim mas tambem terá conteudo informativo, tecnico e que pode ajudar você na sua caminha na programação Web.</div>
+    <div class="content">
+    <p> Ja fazia muito tempo que eu planejava atualizar meu portólio, e tambem sempre tive vontade de apresentar para as pessoas os 
+    novos conceitos e tecnologias que eu aprendia e aplicava nos meu projetos. Então, "juntando o útil ao agradavel" aqui está meu portfólio novo! 
+    Aqui não apenas apresenta informações sobre mim mas tambem terá conteúdos informativos, tecnico e que pode ajudar você na sua caminha na programação Web.</p></div>
     <div class="sub-title">Quem é Guilherme Rodrigues?</div>
     <div class="content"><p> Estudante de Ciencia da Computação e em processo de migração para Analise e Desenvolvimento de Sistemas. 
     Imerso na Area de T.I des de 2017, hoje tenho 23 anos e 5 anos de experiencia de mercado, com meu primeiro trabalho surgindo em 2019. 
@@ -27,20 +28,30 @@ export const articlesList: Articles[] = [
     </ul>  
     </div>
     
-    <div class="sub-title">Oque voce pode encontrar por aqui</div>
+    <div class="sub-title">Oque você pode encontrar por aqui</div>
     <div class="content">
     <p> Aqui no meu portfólio voce vai encontrar conteudos sobre as mais diversas areas do desenvolvimento de softwares. Estou comprometido a toda semana 
-    adicionar um artigo novo para ajudar a solidificar os meus e os seus conhecimentos. Muitos dos topicos abordados por aqui são relacionados ao .Net (C#)
-    mas você tambem pode esperar por conteudos relacionados como Redes de computadores e seu protocolos, Criptografia, Exemplos de aplicações, 
-    Padrões de projetos, Bancos de dados e talvez um pocuo da cultura DevOps.</p>
+    adicionar um artigo novo para ajudar a solidificar os meus e os seus conhecimentos. Muitos dos tópicos abordados por aqui são relacionados ao .Net (C#)
+    mas você tambem pode esperar por conteúdos relacionados como Redes de computadores e seu protocolos, Criptografia, Exêmplos de aplicações, 
+    Padrões de projetos, Bancos de dados e talvez um pouco de DevOps.</p>
 
     </div>
-    <div class="sub-title">Artigos e codigos</div>
+    <div class="sub-title">Artigos e códigos</div>
     <div class="content">
-    <p>Tambem devo estar sempre disponibilizando (quando for o caso) o codigo das aplicações no meu <a href="https://github.com/DevGuilhermeSv" target="_blank"><b> Github</b></a> então 
-    eu ja recomendo dar uma olhadinha por lá para conferir todos os projetos disponiveis
-    e se estiver a vontade, confira também o meu  <a href="https://www.linkedin.com/in/guilherme-silva-6373ab19a/" target="_blank"><b> LinkedIn</b></a> que tambem é aberto para todos</p>
-    </div>`,
+    <p>Tambem devo estar sempre disponibilizando (quando for o caso) o código das aplicações no meu <a href="https://github.com/DevGuilhermeSv" target="_blank"><b> Github</b></a> então 
+    eu já recomendo dar uma olhadinha por lá para conferir todos os projetos disponiveis
+    e se estiver a vontade, confira também o meu  <a href="https://www.linkedin.com/in/guilherme-silva-6373ab19a/" target="_blank"><b> LinkedIn</b></a> que é aberto para todos!</p>
+    </div>
+    <div class="sub-title">Ultimas considerações</div>
+    <div class="content">
+    <p>É isso pessoal... Espero que desfrutem sempre de boas leituras por aqui. Aguardo vocês para acompanhar os proximos topicos e artigos.
+    Se quiser entrar em contato comigo pode ficar a vontade, aceito sempre sugestões. <b> Email: development.guilherme@gmail.com </b>
+
+    </p>
+    <p>    Se você leu até aqui, meu muito obrigado!</p>
+    <p>Tchau tchau :) </p>
+    </div>`
+    ,
     topic: ''
   },
   // {
